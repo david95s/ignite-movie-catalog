0b2aff1b17a5cf3ef264d15b17e661b7d99e25da
David <davidlinconl1@hotmail.com>
2021-03-23 16:04:44 -0300

    Challenge Finalized

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,47 @@
-export function Content() {
-  // Complete aqui
+import {useEffect, useState} from 'react';
+import '../styles/content.scss';
+import { MovieCard } from './MovieCard';
+
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps{
+  movies:MovieProps[];
+  selectedGenre:GenreResponseProps;
+  setMovies:([]) => void;
+}
+
+
+export function Content({movies, setMovies, selectedGenre}:SideBarProps) {
+
+  return (
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  )
 }
